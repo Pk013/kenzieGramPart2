149ae809890af9b48107dbea6e368c110b9bf8f6
morthos <pathshan61@gmail.com>
2018-11-01 15:16:35 -0400

    .

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
-function photoDisplays(imgNames) {
-    let outputString = "";
-    for (let i = 0; i < imgNames.length; i++) {
-      const name = imgNames[i];
-      console.log(name);
-      outputString += `<img src="uploads/${name}"/>`
-    }
-    return outputString;
-  }
-
-  const express = require("express");
+const express = require("express");
 const multer = require("multer");
 const fs = require("fs")
 
@@ -26,11 +16,11 @@ const uploadedFiles = [];
 app.get('/', function (req, res) {
   const path = './public/uploads';
   fs.readdir(path, function (err, items) {
-    res.render('index', { title: 'Kenziegram get pugged', message: 'KenzieGram just got pugged', array: items })
+    res.render('index', { title: 'Kenziegram gettin Pug', message: 'Welcome to Kenziegram With all the pugs', array: items })
   })
 })
 
-function photoDisplays(imgNames) {
+function putUpDatPicture(imgNames) {
   let outputString = "";
   for (let i = 0; i < imgNames.length; i++) {
     const name = imgNames[i];
@@ -38,10 +28,9 @@ function photoDisplays(imgNames) {
   }
   return outputString;
 }
-
 app.post('/uploads', upload.single('myFile'), function (request, response, next) {
   uploadedFiles.push(request.file.filename);
-  response.render('uploads', { title: 'Uploaded Picture With Pug', message: 'Congratulations you have clicked a button to upload a picture!!!', image: request.file.filename });
+  response.render('uploads', { title: 'Uploaded Picture With Pugs', message: 'Congratulations you got pugged', image: request.file.filename });
 })
 
 app.listen(port, () => console.log("Server running on " + port))
---
+++ picturedisplay.js
@@ -0,0 +1,9 @@
+function putUpDatPicture(imgNames) {
+    let outputString = "";
+    for (let i = 0; i < imgNames.length; i++) {
+      const name = imgNames[i];
+      console.log(name);
+      outputString += `<img src="uploads/${name}"/>`
+    }
+    return outputString;
+  }
